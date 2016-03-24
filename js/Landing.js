5f811222c8aa2fb9960b8ffff8986390d386cd31
Juan <jguardado85@gmail.com>
2016-03-24 12:24:59 -0700

    added ShowCard

--- js/Landing.js
+++ js/Landing.js
@@ -1,13 +1,14 @@
 const React = require('react')
+const { Link } = require('react-router')
 
 const Landing = () => (
-    <div className='app-container'>
-      <div className='home-info'>
-        <h1 className='title'>Videos</h1>
-        <input className='search' type='text' placeholder='Search'/>
-        <button className='browse-all'> or Browswe All</button>
-      </div>
+  <div className='app-container'>
+    <div className='home-info'>
+      <h1 className='title'>Videos</h1>
+      <input className='search' type='text' placeholder='Search'/>
+      <Link to='/search' className='browse-all'> or Browswe All</Link>
     </div>
+  </div>
 )
 
 module.exports = Landing
