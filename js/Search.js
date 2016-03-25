d91870bad39f7ca407e4eeda5b569199b29e6233
Juan <jguardado85@gmail.com>
2016-03-25 09:31:35 -0700

    creating a detauls page

--- js/Search.js
+++ js/Search.js
@@ -1,12 +1,16 @@
 const React = require('react')
 const ShowCard = require('./ShowCard')
-const data = require('../public/data')
+const { object } = React.PropTypes
 
 const Search = React.createClass({
   getInitialState () {
     return {
       searchTerm: ''
     }
+  },
+
+  propTypes: {
+    route: object
   },
 
   handleSearchTermEvent (e) {
@@ -21,7 +25,7 @@ const Search = React.createClass({
           <input onChange={this.handleSearchTermEvent} value={this.state.searchTerm} className='search-input' type='text' placeholder='Search'/>
         </header>
         <div className='shows'>
-        {data.shows
+        {this.props.route.shows
           .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
           .map((show) => (
             <ShowCard {...show} key={show.imdbID} />
