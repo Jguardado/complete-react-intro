81ca5da5ba37c5ad2693a72ea468a258b4b904e9
Juan <jguardado85@gmail.com>
2016-03-25 11:39:59 -0700

    begining with flux/redux

--- js/Search.js
+++ js/Search.js
@@ -1,6 +1,7 @@
 const React = require('react')
 const ShowCard = require('./ShowCard')
 const { object } = React.PropTypes
+const Header = require('./Header')
 
 const Search = React.createClass({
   getInitialState () {
@@ -13,17 +14,18 @@ const Search = React.createClass({
     route: object
   },
 
-  handleSearchTermEvent (e) {
-    this.setState({ searchTerm: e.target.value })
+  handleSearchTermChange (searchTerm) {
+    this.setState({ searchTerm })
   },
 
   render () {
     return (
       <div className='container'>
-        <header className='header'>
-          <h1 className='brand'>Videos</h1>
-          <input onChange={this.handleSearchTermEvent} value={this.state.searchTerm} className='search-input' type='text' placeholder='Search'/>
-        </header>
+        <Header
+          handleSearchTermChange={this.handleSearchTermChange}
+          searchTerm={this.state.searchTerm}
+          showSearch
+          />
         <div className='shows'>
         {this.props.route.shows
           .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
